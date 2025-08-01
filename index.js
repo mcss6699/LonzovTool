c9444b76a8c7d7a19c0d1c05c00abe67bd2c9b9a
LonzoV <mcss6699@qq.com>
2025-08-02 01:17:52 +0800

    修复混合内容导致的加载问题

--- index.js
+++ index.js
@@ -160,7 +160,7 @@ const cardsData = [
     { title: "MT管理器↗", description: "Android 平台文件管理 & 逆向修改神器", categories:["friend-links"], link: "https://mt2.cn/", newtap:true },
     { title: "苦力怕论坛↗", description: "国内最大的基岩版资源分享社区(应该是吧)", categories:["friend-links"], link: "https://klpbbs.com/", newtap:true },
     { title: "命令模拟器↗", description: "强大的基岩版命令调试程序", categories:["friend-links"], link: "https://commandsimulator.great-site.net/", newtap:true },
-    { title: "网易版导入", description: "限免活动中…站长自营无坑", categories:["resource"], link: "./dr/pro.html", newtap:true },
+    { title: "网易版导入", description: "限免活动中…站长自营无坑", categories:["resource"], link: "https://tool.lonzov.top/dr/pro", newtap:true },
     { title: "安卓基岩版下载↗", description: "优质的境外下载站", categories:["resource"], link: "https://mcapks.net/", newtap:true },
     { title: "MT管理器-mc语法包", description: "指令语法高亮，压缩包里有具体使用说明", categories:["resource"], link: "./down/mc语法包", newtap:true },
     { title: "幸运转盘", description: "选择困难症？让转盘来决定！", categories:["other"], link: "./spin-the-wheel", newtap:true },
