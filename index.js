eebd08df3bda1d3f917d705980dbca8cf483eaec
LonzoV <mcss6699@qq.com>
2025-08-08 19:58:56 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -149,7 +149,7 @@ const cardsData = [
     { title: "T显动画", description: "自动生成“打字机”动画", categories:["command"], link: "./c/tr", newtap:false },
     { title: "T显编辑器↗", description: "可视化T显编辑器 · by Dislink", categories:["command"], link: "https://dislink.github.io/rawJSONEditor/", newtap:true },
     { title: "语法转换", description: "execute指令语法升级", categories:["command"], link: "./c/execute", newtap:false },
-    { title: "CHelper↗", description: "命令助手在线版", categories:["command"], link: "https://www.yanceymc.cn/chelper/", newtap:false },
+    { title: "CHelper↗", description: "命令助手在线版", categories:["command"], link: "https://www.yanceymc.cn/chelper/", newtap:true },
     { title: "特殊符号↗", description: "特殊符号大全", categories:["command"], link: "https://teshuzifu.cn/", newtap:true },
     { title: "MC特殊符号", description: "mc特殊符号，如鸡腿M币等", categories:["command"], link: "./c/fuhao", newtap:false },
     { title: "MCNav↗", description: "MC导航站 | MC工具大全", categories:["friend-links"], link: "https://www.mcnav.net/", newtap:true },
